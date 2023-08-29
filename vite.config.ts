359e20acef56ddf8fb065bce6681c898d38a2af1
xueke-alan <xueke.alan@gmail.com>
2023-08-30 00:37:39 +0800

    8.30

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,6 @@ export default defineConfig({
   server: {
     // host:'0.0.0.0' ,//ip地址
     port: 5678, // 设置服务启动端口号
-    open: true, // 设置服务启动时是否自动打开浏览器
+    // open: true, // 设置服务启动时是否自动打开浏览器
   },
 });
